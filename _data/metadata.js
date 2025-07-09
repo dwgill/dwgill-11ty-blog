799943d899b2eef53ec9cd2c70129a3b73940d2a
Daniel Gill <dwgill@outlook.com>
2025-07-08 22:12:53 -0400

    Update _data/metadata.js (via Pages CMS)

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
+	title: "Design Patterns & Dice Bags",
+	url: "https://blog-11ty.dwgill.com/",
 	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
+	description: "Personal blog of Daniel Gill.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "Daniel Gill",
+		email: "dwgill@outlook.com",
+		url: "https://blog-11ty.dwgill.com/about-me/"
 	}
 }
